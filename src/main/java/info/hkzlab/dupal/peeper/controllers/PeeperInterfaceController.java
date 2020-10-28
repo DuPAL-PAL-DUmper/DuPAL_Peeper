ea9e396c5da9fe52733783bf83933337b13c9594
Fabio Battaglia <tabaglio@posteo.net>
2020-10-28 19:13:06 +0100

    Simplify the interface a bit

--- src/main/java/info/hkzlab/dupal/peeper/controllers/PeeperInterfaceController.java
+++ src/main/java/info/hkzlab/dupal/peeper/controllers/PeeperInterfaceController.java
@@ -1,24 +1,26 @@
 package info.hkzlab.dupal.peeper.controllers;
 
+import info.hkzlab.dupal.peeper.App;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 
 public class PeeperInterfaceController {
     @FXML private ImageView palModelPicture;
+
     @FXML private ListView<String> eventHistoryList;
-    @FXML private TextField cmdInputField;
+
     @FXML private GridPane writeGridPane;
     @FXML private GridPane readGridPane;
+
     @FXML private Button writeRstButton;
     @FXML private Button writeClkButton;
     @FXML private Button writeSetButton;
+
     @FXML private Button readResetButton;
 
     public void initialize() {
-
     } 
 }
