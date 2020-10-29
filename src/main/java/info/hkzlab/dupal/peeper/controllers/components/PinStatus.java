122ae2582e6e5e8d5ba9018f95927482b931fe37
Fabio Battaglia <tabaglio@posteo.net>
2020-10-29 17:19:25 +0100

    Better define the PinStatus

--- src/main/java/info/hkzlab/dupal/peeper/controllers/components/PinStatus.java
+++ src/main/java/info/hkzlab/dupal/peeper/controllers/components/PinStatus.java
@@ -2,20 +2,32 @@ package info.hkzlab.dupal.peeper.controllers.components;
 
 public class PinStatus {
    public final int pinNumber;
+   public final int index;
    public final String pinName;
 
    private boolean status = false;
+   private boolean previousStatus = false;
 
-   public PinStatus(int pinNumber, String pinName) {
+   public PinStatus(int pinNumber, int index, String pinName) {
         this.pinNumber = pinNumber;
         this.pinName = pinName;
+        this.index = index;
    }
 
    public void setStatus(boolean status) {
+       previousStatus = this.status;
        this.status = status;
    }
 
    public boolean getStatus() {
        return status;
+   }
+
+   public boolean wasModified() {
+       return status != previousStatus;
+   }
+
+   public void commitChange() {
+       previousStatus = status;
    }
 }
